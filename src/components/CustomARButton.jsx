cb01eab640370e4d225f7f864ce29ac93d8893ee
Astraalito <nicolas.asri@gmail.com>
2025-03-26 17:13:59 +0100

    Added dealing with non-compatible devices

--- src/components/CustomARButton.jsx
+++ src/components/CustomARButton.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { startSession } from "@react-three/xr";
 
 const CustomARButton = () => {
+	const [isARCompatible, setIsARCompatible] = useState(false);
+
+	useEffect(() => {
+		const checkARCompatibility = async () => {
+			if (navigator.xr) {
+				try {
+					const session = await navigator.xr.isSessionSupported('immersive-ar');
+					setIsARCompatible(session);
+				} catch (error) {
+					console.error('AR is not supported by this device.', error);
+					setIsARCompatible(false);
+				}
+			} else {
+				console.error('WebXR is not supported by this browser.');
+				setIsARCompatible(false);
+			}
+		};
+
+		checkARCompatibility();
+	}, []);
+
 	const handleStartSession = async () => {
-		await startSession('immersive-ar', {
-			requiredFeatures: ["hit-test"],
-			optionalFeatures: ["dom-overlay"],
-			domOverlay: { root: document.body }
-		});
+		if (isARCompatible) {
+			await startSession('immersive-ar', {
+				requiredFeatures: ["hit-test"],
+				optionalFeatures: ["dom-overlay"],
+				domOverlay: { root: document.body }
+			});
+		}
 	};
 
 	return (
-		<button onClick={handleStartSession} className="round-button button-startAR">
+		<button
+			onClick={handleStartSession}
+			className={`round-button button-startAR ${isARCompatible ? '' : 'disabled'}`}
+			disabled={!isARCompatible}
+		>
 			<img src="img/svg/start-ar.svg" alt="AR Icon" className="ar-icon button-icon big-icon" />
-			Découvrir en AR !
+			{isARCompatible ? 'Découvrir en AR !' : 'AR non pris en charge'}
 		</button>
 	);
 };
